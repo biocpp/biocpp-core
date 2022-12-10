a74917b3aabfa29f9b36cd079a05da95b5b4cc4c
Hannes Hauswedell <hannes.hauswedell@decode.is>
2022-12-10 14:47:09 +0100

    [feature] views::to_char compat with cigar

--- include/bio/ranges/views/to_char.hpp
+++ include/bio/ranges/views/to_char.hpp
@@ -15,6 +15,7 @@
 
 #include <ranges>
 
+#include <bio/alphabet/cigar/cigar.hpp>
 #include <bio/alphabet/concept.hpp>
 #include <bio/alphabet/custom/all.hpp>
 #include <bio/meta/overloaded.hpp>
@@ -40,37 +41,57 @@ namespace bio::ranges::views
  *
  * \header_file{bio/ranges/views/to_char.hpp}
  *
- * ### View properties
+ * ### View properties (generic case)
  *
  * This view is a **deep view** Given a range-of-range as input (as opposed to just a range), it will apply
  * the transformation on the innermost range (instead of the outermost range).
  *
- * | Concepts and traits              | `urng_t` (underlying range type)      | `rrng_t` (returned range type)                                |
- * |----------------------------------|:-------------------------------------:|:-------------------------------------------------------------:|
- * | std::ranges::input_range         | *required*                            | *preserved*                                                   |
- * | std::ranges::forward_range       |                                       | *preserved*                                                   |
- * | std::ranges::bidirectional_range |                                       | *preserved*                                                   |
- * | std::ranges::random_access_range |                                       | *preserved*                                                   |
- * | std::ranges::contiguous_range    |                                       | *lost*¹                                                       |
- * |                                  |                                       |                                                               |
- * | std::ranges::viewable_range      | *required*                            | *guaranteed*                                                  |
- * | std::ranges::view                |                                       | *guaranteed*                                                  |
- * | std::ranges::sized_range         |                                       | *preserved*                                                   |
- * | std::ranges::common_range        |                                       | *preserved*                                                   |
- * | std::ranges::output_range        |                                       | *lost*¹                                                       |
- * | bio::ranges::const_iterable_range|                                       | *preserved*                                                   |
- * |                                  |                                       |                                                               |
- * | std::ranges::range_reference_t   | bio::alphabet::alphabet               | bio::alphabet::char_t<std::ranges::range_value_t<urng_t>>     |
- *
- * ¹ See exceptions below.
+ * | Concepts and traits              | `urng_t` (underlying range type)  | `rrng_t` (returned range type)                           |
+ * |----------------------------------|:---------------------------------:|:--------------------------------------------------------:|
+ * | std::ranges::input_range         | *required*                        | *preserved*                                              |
+ * | std::ranges::forward_range       |                                   | *preserved*                                              |
+ * | std::ranges::bidirectional_range |                                   | *preserved*                                              |
+ * | std::ranges::random_access_range |                                   | *preserved*                                              |
+ * | std::ranges::contiguous_range    |                                   | *lost*                                                   |
+ * |                                  |                                   |                                                          |
+ * | std::ranges::viewable_range      | *required*                        | *guaranteed*                                             |
+ * | std::ranges::view                |                                   | *guaranteed*                                             |
+ * | std::ranges::sized_range         |                                   | *preserved*                                              |
+ * | std::ranges::common_range        |                                   | *preserved*                                              |
+ * | std::ranges::output_range        |                                   | *lost*                                                   |
+ * | bio::ranges::const_iterable_range|                                   | *preserved*                                              |
+ * |                                  |                                   |                                                          |
+ * | std::ranges::range_reference_t   | bio::alphabet::alphabet           | bio::alphabet::char_t<std::ranges::range_value_t<urng_t>>|
  *
  * See the \link views views submodule documentation \endlink for detailed descriptions of the view properties.
  *
- * ### Special cases
+ * ### View properties (NOOP case)
  *
- * **Strings:** ¹ If range is given whose value_type is the same it's alphabet type (e.g. std::string), a simple view to the range
- * is returned and no transformation happens. In that case, both, contiguous-ness and writeability are preserved.
- * Thus, there is no overhead when applying this adaptor to strings or string_views.
+ * If a range is given whose value_type is the same its alphabet type (e.g. std::string), a simple view
+ * to the range is returned and no transformation happens. See bio::ranges::views::type_reduce.
+ *
+ *
+ * ### View properties (bio::alphabet::cigar)
+ *
+ * This range adaptor can be applied to ranges over bio::alphabet::cigar (although that type does not
+ * model bio::alphabet::alphabet). The returned view is weaker than in the generic case:
+ *
+ * | Concepts and traits              | `urng_t` (underlying range type)  | `rrng_t` (returned range type)   |
+ * |----------------------------------|:---------------------------------:|:--------------------------------:|
+ * | std::ranges::input_range         | *required*                        | *preserved*                      |
+ * | std::ranges::forward_range       |                                   | *lost*                           |
+ * | std::ranges::bidirectional_range |                                   | *lost*                           |
+ * | std::ranges::random_access_range |                                   | *lost*                           |
+ * | std::ranges::contiguous_range    |                                   | *lost*                           |
+ * |                                  |                                   |                                  |
+ * | std::ranges::viewable_range      | *required*                        | *guaranteed*                     |
+ * | std::ranges::view                |                                   | *guaranteed*                     |
+ * | std::ranges::sized_range         |                                   | *lost*                           |
+ * | std::ranges::common_range        |                                   | *lost*                           |
+ * | std::ranges::output_range        |                                   | *lost*                           |
+ * | bio::ranges::const_iterable_range|                                   | *lost*                           |
+ * |                                  |                                   |                                  |
+ * | std::ranges::range_reference_t   | bio::alphabet::cigar              | char                             |
  *
  * ### Example
  * \include test/snippet/ranges/views/range_view_to_char.cpp
@@ -78,6 +99,18 @@ namespace bio::ranges::views
  */
 inline auto const to_char = detail::adaptor_from_functor{meta::overloaded{
   // clang-format off
+    // cigar special case
+    []<std::ranges::input_range rng_t>(rng_t && range)
+        requires(std::same_as<ranges::range_innermost_value_t<rng_t>, bio::alphabet::cigar>)
+    {
+        //TODO we should mark small_strings <= 32 bytes as views and just return them here
+        auto fn = [sbuf = small_string<10>{} ] (bio::alphabet::cigar const c) mutable
+        {
+            return c.to_string(sbuf);
+        };
+        return std::forward<rng_t>(range) | deep{std::views::transform(fn) | std::views::join};
+    },
+    // char2char special case
     []<std::ranges::input_range rng_t>(rng_t && range)
         requires(alphabet::alphabet<ranges::range_innermost_value_t<rng_t>> &&
                  std::same_as<ranges::range_innermost_value_t<rng_t>,
@@ -85,11 +118,13 @@ inline auto const to_char = detail::adaptor_from_functor{meta::overloaded{
     {
         return std::forward<rng_t>(range) | views::type_reduce; // NOP
     },
+    // generic case
     []<std::ranges::input_range rng_t>(rng_t && range)
         requires(alphabet::alphabet<ranges::range_innermost_value_t<rng_t>>)
     {
         return std::forward<rng_t>(range) | deep{std::views::transform(alphabet::to_char)};
     },
+    // catch-all
     []<typename rng_t>(rng_t &&)
     {
         static_assert(meta::always_false<rng_t>,
--- test/unit/ranges/views/view_to_char_test.cpp
+++ test/unit/ranges/views/view_to_char_test.cpp
@@ -7,6 +7,7 @@
 // -----------------------------------------------------------------------------------------------------
 
 #include <iostream>
+#include <iterator>
 #include <ranges>
 
 #include <gtest/gtest.h>
@@ -15,6 +16,7 @@
 #include <bio/ranges/concept.hpp>
 #include <bio/ranges/to.hpp>
 #include <bio/ranges/views/to_char.hpp>
+#include <bio/test/expect_range_eq.hpp>
 
 using namespace bio::alphabet::literals;
 
@@ -39,12 +41,53 @@ TEST(view_to_char, basic)
 
 TEST(view_to_char, preserve_string)
 {
-    std::string const s{"ACTTTGATA"};
+    {
+        std::string const s{"ACTTTGATA"};
 
-    auto v = s | bio::ranges::views::to_char;
+        auto v = s | bio::ranges::views::to_char;
 
-    EXPECT_EQ(v, s);
-    EXPECT_TRUE((std::same_as<decltype(v), std::string_view>));
+        EXPECT_EQ(v, s);
+        EXPECT_TRUE((std::same_as<decltype(v), std::string_view>));
+    }
+
+    /* deep behaviour */
+    {
+        std::vector<std::string> vec{"ACGT", "AGGA"};
+        auto                     v = vec | bio::ranges::views::to_char;
+
+        EXPECT_RANGE_EQ(v, vec);
+        EXPECT_TRUE((std::same_as<decltype(v), std::span<std::string>>));
+    }
+}
+
+TEST(view_to_char, cigars){
+#ifndef __clang__
+  {std::vector<bio::alphabet::cigar> vec{{23, 'I'_cigar_op}, {42, 'M'_cigar_op}, {42, 'D'_cigar_op}};
+
+std::string_view comp = "23I42M42D";
+
+auto v = vec | bio::ranges::views::to_char;
+
+static_assert(std::ranges::input_range<decltype(v)>);
+EXPECT_RANGE_EQ(v, comp);
+}
+
+/* deep */
+{
+    std::vector<std::vector<bio::alphabet::cigar>> vec{
+      {{23, 'I'_cigar_op}, {42, 'M'_cigar_op}, {42, 'D'_cigar_op}},
+      { {1, 'I'_cigar_op}, {22, 'M'_cigar_op}, {33, 'D'_cigar_op}}
+    };
+
+    std::vector<std::string_view> comp = {"23I42M42D", "1I22M33D"};
+
+    auto v = vec | bio::ranges::views::to_char;
+
+    ASSERT_EQ(v.size(), 2ull);
+    EXPECT_RANGE_EQ(v[0], comp[0]);
+    EXPECT_RANGE_EQ(v[1], comp[1]);
+}
+#endif
 }
 
 TEST(view_to_char, concepts)
@@ -86,4 +129,19 @@ TEST(view_to_char, concepts)
     EXPECT_TRUE(std::ranges::common_range<decltype(v2)>);
     EXPECT_TRUE(bio::ranges::const_iterable_range<decltype(v2)>);
     EXPECT_TRUE((std::ranges::output_range<decltype(v2), char>));
+
+#ifndef __clang__
+    std::vector<bio::alphabet::cigar> cig;
+    auto                              v3 = cig | bio::ranges::views::to_char;
+    EXPECT_TRUE(std::ranges::input_range<decltype(v3)>);
+    EXPECT_FALSE(std::ranges::forward_range<decltype(v3)>);       // TODO should be TRUE
+    EXPECT_FALSE(std::ranges::bidirectional_range<decltype(v3)>); // TODO should be TRUE
+    EXPECT_FALSE(std::ranges::random_access_range<decltype(v3)>);
+    EXPECT_FALSE(std::ranges::contiguous_range<decltype(v3)>);
+    EXPECT_TRUE(std::ranges::view<decltype(v3)>);
+    EXPECT_FALSE(std::ranges::sized_range<decltype(v3)>);
+    EXPECT_FALSE(std::ranges::common_range<decltype(v3)>);
+    EXPECT_FALSE(bio::ranges::const_iterable_range<decltype(v3)>); // TODO should be TRUE
+    EXPECT_FALSE((std::ranges::output_range<decltype(v3), char>));
+#endif
 }
