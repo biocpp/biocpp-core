52a5ef04dd45cbb0f7d4255a9bcb2d5e6607dabf
Hannes Hauswedell <h2+github.com@fsfe.org>
2023-05-03 17:38:21 +0000

    [misc] simplify bio::ranges::dictionary

--- test/snippet/ranges/container/dictionary.cpp
+++ test/snippet/ranges/container/dictionary.cpp
@@ -11,18 +11,18 @@ int main()
     employee_status.emplace_back("Maria", "promoted");
 
     /* Access by position in dictionary; same as insert-order */
-    auto && first = employee_status[0];         // a tuple of key and mapped_value
-    fmt::print("{}\n", first);                  // prints '("Bob", "hired")'
-    //get<0>(first) = "Rob";                    // key cannot be changed
-    get<1>(first) = "tired";                    // mapped_value can be changed
-    fmt::print("{}\n", employee_status[0]);     // prints '("Bob", "tired")'
+    auto && first = employee_status[0];             // a tuple of key and mapped_value
+    fmt::print("{}\n", first);                      // prints '("Bob", "hired")'
+    //get<0>(first) = "Rob";                        // key cannot be changed
+    get<1>(first) = "tired";                        // mapped_value can be changed
+    fmt::print("{}\n", employee_status[0]);         // prints '("Bob", "tired")'
 
     /* Access by key */
-    auto && jane = employee_status["Jane"];     // the mapped_value belonging to Jane
-    fmt::print("{}\n", jane);                   // prints 'hired'
-    jane = "wired";                             // can be changed
-    fmt::print("{}\n", employee_status["Jane"]);// prints 'wired'
+    auto && janes_status = employee_status["Jane"]; // the mapped_value belonging to Jane
+    fmt::print("{}\n", janes_status);               // prints 'hired'
+    janes_status = "wired";                         // can be changed
+    fmt::print("{}\n", employee_status["Jane"]);    // prints 'wired'
 
     /* No implicit inserts like std::unordered_map */
-    //employee_status["Shane"] = "hired";       // DOES NOT WORK.
+    //employee_status["Shane"] = "hired";           // DOES NOT WORK.
 }
